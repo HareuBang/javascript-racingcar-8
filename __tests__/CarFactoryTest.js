664584d7816510b58a59abe1dc3a592f5660a730
HareuBang <divjae7124@gmail.com>
2025-10-23 21:33:40 +0900

    test: CarFactory를 통한 Car 생성 및 동작 확인 테스트 코드 작성

---
+++ __tests__/CarFactoryTest.js
@@ -0,0 +1,55 @@
+import CarFactory from "../src/CarFactory.js";
+
+describe("CarFactory를 통해 생성된 Car 동작 확인", () => {
+  describe("자동차 생성 기능 테스트", () => {
+    test.each([
+      ["pobi"],
+      ["woni"],
+      ["jun"],
+      ["👍👍"],
+      [" ⭐ "],
+      [" ^_^ "],
+      [" ㅠ ㅠ "],
+    ])(
+      "%s의 이름으로 자동차를 생성 시 이름과 거리가 올바르게 초기화된 Car 인스턴스가 생성된다.",
+      (inputName) => {
+        const car = CarFactory.createCar(inputName);
+
+        expect(car.getName()).toBe(inputName);
+        expect(car.getDistance()).toBe(0);
+      }
+    );
+  });
+
+  describe("자동차 전진 기능 테스트", () => {
+    test("생성된 자동차를 전진시키면 이동거리가 1 증가합니다.", () => {
+      const car = CarFactory.createCar("pobi");
+
+      car.forward();
+      expect(car.getDistance()).toBe(1);
+
+      car.forward();
+      expect(car.getDistance()).toBe(2);
+    });
+  });
+
+  describe("예외 테스트", () => {
+    test.each([
+      [null, "자동차 이름들은 문자열만 입력 가능합니다."],
+      [undefined, "자동차 이름들은 문자열만 입력 가능합니다."],
+      [["pobi", "woni", "jun"], "자동차 이름들은 문자열만 입력 가능합니다."],
+      [{ name: "pobi" }, "자동차 이름들은 문자열만 입력 가능합니다."],
+      [123, "자동차 이름들은 문자열만 입력 가능합니다."],
+      [true, "자동차 이름들은 문자열만 입력 가능합니다."],
+      ["", "자동차 이름은 최소 1글자 이상만 가능합니다."],
+      ["     ", "자동차 이름은 최소 1글자 이상만 가능합니다."],
+      ["123456", "자동차 이름은 5글자 이하만 가능합니다."],
+      ["이것은 자동차 이름입니다.", "자동차 이름은 5글자 이하만 가능합니다."],
+    ])(
+      "잘못된 자동차 이름 `%s`을(를) 입력하면 `%s` 에러 메시지를 반환한다.",
+      (inputName, expected) => {
+        expect(() => CarFactory.createCar(inputName)).toThrow(expected);
+      }
+    );
+  });
+});
