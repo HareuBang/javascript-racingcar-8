8f05264283974a715ed75dc1c5f4de383873da20
HareuBang <divjae7124@gmail.com>
2025-10-23 21:24:14 +0900

    refactor: Car 클래스의 매직 넘버 및 문자열 상수화

--- src/Car.js
+++ src/Car.js
@@ -1,4 +1,5 @@
 import { CAR_NAME_ERRORS } from "./constants/errorMessage";
+import { CAR } from "./constants/carConstants";
 class Car {
   #name;
   #distance;
@@ -7,19 +8,21 @@ class Car {
     this.#validateName(name);
 
     this.#name = name;
-    this.#distance = 0;
+    this.#distance = CAR.INITIAL_DISTANCE_ZERO;
   }
 
   #validateName(name) {
-    if (typeof name !== "string") throw new Error(CAR_NAME_ERRORS.NOT_STRING);
-    if (name.length === 0 || /\s{5}/.test(name))
+    if (typeof name !== CAR.NAME_TYPE_STRING)
+      throw new Error(CAR_NAME_ERRORS.NOT_STRING);
+    // 빈 문자열 또는 공백만 있는 경우
+    if (name.length === CAR.NAME_EMPTY || CAR.NAME_ALL_SPACE_REGEXP.test(name))
       throw new Error(CAR_NAME_ERRORS.NOT_EMPTY);
-    if (!(name.length <= 5))
+    if (!(name.length <= CAR.NAME_MAX_LENGTH_FIVE))
       throw new Error(CAR_NAME_ERRORS.NOT_MAX_LENGTH_FIVE);
   }
 
   forward() {
-    this.#distance += 1;
+    this.#distance += CAR.DISTANCE_INCREMENT_ONE;
   }
 
   getName() {
