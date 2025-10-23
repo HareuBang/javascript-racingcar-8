9b66eb09e29d10d9c44e767868aaebcf08aeea72
HareuBang <divjae7124@gmail.com>
2025-10-23 17:37:16 +0900

    feat: 시도할 횟수를 입력받는 기능을 inputView에 추가 및 입력 메시지 상수화

--- src/console/inputView.js
+++ src/console/inputView.js
@@ -1,11 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { PARTICIPANT_CAR_NAMES_PROMPT } from "../constants/message.js";
+import {
+  PARTICIPANT_CAR_NAMES_PROMPT,
+  RACE_LAPS_PROMPT,
+} from "../constants/message.js";
 
 const readLineAsync = async (question) =>
   await MissionUtils.Console.readLineAsync(question);
 
 const inputView = {
   readCarNames: async () => await readLineAsync(PARTICIPANT_CAR_NAMES_PROMPT),
+  readRaceLaps: async () => await readLineAsync(RACE_LAPS_PROMPT),
 };
 
 export default inputView;
--- src/constants/message.js
+++ src/constants/message.js
@@ -1,4 +1,5 @@
 const PARTICIPANT_CAR_NAMES_PROMPT =
   "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+const RACE_LAPS_PROMPT = "시도할 횟수는 몇 회인가요?\n";
 
-export { PARTICIPANT_CAR_NAMES_PROMPT };
+export { PARTICIPANT_CAR_NAMES_PROMPT, RACE_LAPS_PROMPT };
