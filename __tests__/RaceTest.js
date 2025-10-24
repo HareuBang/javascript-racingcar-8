345a7a60c7f7aa05a42e1e172079cd2e4e3e452a
HareuBang <divjae7124@gmail.com>
2025-10-24 16:47:37 +0900

    refactor: test.each()를 사용하여 예외 테스트 케이스를 개별화
    - 한 개의 테스트 이름으로 여러 expect가 묶여 실패 시 원인을 파악하기 어려워
    test.each()를 사용하여 예외 테스트 케이스를 개별화해서 이를 통해 어떤 입력 값
    이 어떤 실패를 발생시키는 지 테스트 실행 결과에서 명확하게 확인할 수 있기 때문
    에 test.eact()를 사용했습니다.

--- __tests__/RaceTest.js
+++ __tests__/RaceTest.js
@@ -21,38 +21,31 @@ describe("Race", () => {
   });
 
   describe("예외 테스트", () => {
-    test("유효하지 않은 자동차 이름을 입력하면 에러를 반환한다.", () => {
-      const race = new Race(mockCarFactory);
+    test.each([
+      [null, "1", "문자열로만 입력 가능합니다."],
+      [["pobi", "woni", "jun"], "5", "문자열로만 입력 가능합니다."],
+      ["", "2", "최소 1글자 이상만 가능합니다."],
+      ["    ", "4", "최소 1글자 이상만 가능합니다."],
+    ])(
+      "유효하지 않은 자동차 이름 `%s`을(를) 입력하면 %s 에러를 반환한다.",
+      (name, laps, expected) => {
+        const race = new Race(mockCarFactory);
+        expect(() => race.prepare(name, laps)).toThrow(expected);
+      }
+    );
 
-      expect(() => race.prepare(null, "1")).toThrow(
-        "자동차 이름들은 문자열로만 입력 가능합니다."
-      );
-      expect(() => race.prepare(["pobi", "woni", "jun"], "5")).toThrow(
-        "자동차 이름들은 문자열로만 입력 가능합니다."
-      );
-      expect(() => race.prepare("", "3")).toThrow(
-        "자동차 이름은 최소 1글자 이상만 가능합니다."
-      );
-      expect(() => race.prepare(" ", "2")).toThrow(
-        "자동차 이름은 최소 1글자 이상만 가능합니다."
-      );
-    });
-
-    test("유효하지 않은 시도할 횟수를 입력하면 에러를 반환한다.", () => {
-      const race = new Race(mockCarFactory);
-
-      expect(() => race.prepare("pobi,woni,jun", [1])).toThrow(
-        "시도할 횟수는 숫자만 입력 가능합니다."
-      );
-      expect(() => race.prepare("pobi,woni,jun", undefined)).toThrow(
-        "시도할 횟수는 숫자만 입력 가능합니다."
-      );
-      expect(() => race.prepare("pobi,woni,jun", "-3")).toThrow(
-        "시도할 횟수는 양수만 입력 가능합니다."
-      );
-      expect(() => race.prepare("pobi,woni,jun", "0")).toThrow(
-        "시도할 횟수는 양수만 입력 가능합니다."
-      );
-    });
+    test.each([
+      [[1], "시도할 횟수는 숫자만 입력 가능합니다."],
+      [undefined, "시도할 횟수는 숫자만 입력 가능합니다."],
+      ["-3", "시도할 횟수는 양수만 입력 가능합니다."],
+      ["0", "시도할 횟수는 양수만 입력 가능합니다."],
+      ["3.14", "시도할 횟수는 양수만 입력 가능합니다."],
+    ])(
+      "유효하지 않은 자동차 이름 `%s`을(를) 입력하면 %s 에러를 반환한다.",
+      (laps, expected) => {
+        const race = new Race(mockCarFactory);
+        expect(() => race.prepare("pobi,woni,jun", laps)).toThrow(expected);
+      }
+    );
   });
 });
