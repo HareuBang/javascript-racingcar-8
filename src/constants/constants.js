22488f8d4b46dcf8db9d0208dbe6a969a6f42251
HareuBang <divjae7124@gmail.com>
2025-10-24 17:12:47 +0900

    rename: carConstants.js 파일명을 constants.js 파일명으로 변경
    - Race 클래스와 Car 클래스에서 공통적으로 사용하는 상수가 있으며 이로 인해
    carConstants.js 파일명보다는 상수 관리라는 의미를 더 명확하게 하기 위해 파일
    명을 변경했습니다.

---
+++ src/constants/constants.js
@@ -0,0 +1,19 @@
+export const CAR_DISTANCE = {
+  INITIAL_ZERO: 0,
+  INCREMENT_ONE: 1,
+};
+
+export const CAR_NAME = {
+  MAX_LENGTH_FIVE: 5,
+  SEPARATOR: ",",
+};
+
+export const VALID_TYPE = {
+  STRING: "string",
+  EMPTY: "",
+  NUMBER: "number",
+};
+
+export const LAPS = {
+  MIN_NUMBER_ONE: 1,
+};
