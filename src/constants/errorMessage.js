08c396dee440c923ef649f1976a8cec683b23ab2
HareuBang <divjae7124@gmail.com>
2025-10-24 17:50:13 +0900

    refactor: 공통 검증 로직 적용을 위해 자동차 이름 오류 메시지 수정

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,7 +1,7 @@
 export const ERROR_PREFIX = "[ERROR] ";
 export const CAR_NAME_ERRORS = {
-  NOT_STRING: "자동차 이름들은 문자열로만 입력 가능합니다.",
-  NOT_EMPTY: "자동차 이름은 최소 1글자 이상만 가능합니다.",
+  NOT_STRING: "문자열로만 입력 가능합니다.",
+  NOT_EMPTY: "최소 1글자 이상만 가능합니다.",
   NOT_MAX_LENGTH_FIVE: "자동차 이름은 5자 이하만 가능합니다.",
 };
 export const LAPS_ERROR = {
