aa3294d8784738fa5dd986bab7dc70b6053aa4fb
HareuBang <divjae7124@gmail.com>
2025-10-24 17:18:31 +0900

    refactor: Race 클래스에서 laps 입력 검증에 대한 에러 메시지 상수화

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -4,3 +4,7 @@ export const CAR_NAME_ERRORS = {
   NOT_EMPTY: "자동차 이름은 최소 1글자 이상만 가능합니다.",
   NOT_MAX_LENGTH_FIVE: "자동차 이름은 5자 이하만 가능합니다.",
 };
+export const LAPS_ERROR = {
+  NOT_NUMBER: "시도할 횟수는 숫자만 입력 가능합니다.",
+  POSITIVE_NUMBER: "시도할 횟수는 1 이상의 양수만 입력 가능합니다.",
+};
