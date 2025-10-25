8adf498f2554e987958fc66f109f50aef5452680
HareuBang <divjae7124@gmail.com>
2025-10-26 01:50:46 +0900

    refactor: CarFactory 구조를 확장 가능한 type 기반 생성 방식으로 개선
    - 기존의 createCar()만 RaceTrack 생성자에 주입하여 사용했는 데 이는 정적 팩
    토리 메서드의 다른 종류의 자동차가 추가되더라도 기존 코드의 수정 없이 유연하게
    확장할 수 있는 유연한 확장성과 장점(특징)을 충분히 드러내지 못했기 때문에 명확
    히 드러나도록 구조를 개선했습니다.

--- src/CarFactory.js
+++ src/CarFactory.js
@@ -1,7 +1,14 @@
 import Car from "./Car";
-
 export class CarFactory {
-  static createCar(name) {
+  static #createCar(name) {
     return new Car(name);
+  }
+
+  static create(type = "normal") {
+    const carType = {
+      normal: this.#createCar(),
+    };
+
+    return carType[type];
   }
 }
