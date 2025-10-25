8c2168e99139dc1bb9818eeaab01e1eefabeb23b
HareuBang <divjae7124@gmail.com>
2025-10-26 02:17:01 +0900

    feat: App 클래스에 의존성 주입을 통한 Application 실행 환경 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import inputView from "./console/inputView";
+import { outputView } from "./console/outputView";
+import { CarFactory } from "./CarFactory";
+import { Race } from "./Race";
+import { RaceTrack } from "./RaceTrack";
 class App {
-  async run() {}
+  async run() {
+    const race = new Race(CarFactory.create);
+    const raceTrack = new RaceTrack(inputView, race, outputView);
+    raceTrack.race();
+  }
 }
 
 export default App;
