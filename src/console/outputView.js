17c6196e2adabc01dcf68ef3a7dd1071def7e82d
HareuBang <divjae7124@gmail.com>
2025-10-26 04:08:27 +0900

    style: 요구사항에 명시된 출력 형식을 따르기 위해 outputView 출력 형식 개선
    - "시도할 횟수는 몇 회인가요?" 입력 질문과 "실행 결과" 출력 메시지 사이에
    출력이 서로 붙지 않도록 요구사항에 명시된 출력 형식에 맞게 1칸 줄바꿈을 추가했
    습니다.
    - 실행 결과와 "최종 우승자" 사이에 발생하던 불필요한 2칸 줄바꿈을 제거
    하고 요구사항에 명시된 출력 형식에 맞게 1칸 줄바꿈이 되도록 수정했습니다.

--- src/console/outputView.js
+++ src/console/outputView.js
@@ -4,9 +4,9 @@ import { PRINT } from "../constants/message.js";
 const print = (message) => MissionUtils.Console.print(message);
 
 const outputView = {
-  printScoreBoard: () => print(PRINT.EXECUTION_RESULT_TITLE),
-  printExecutionResult: (executionResult) =>
-    print(executionResult + PRINT.NEWLINE_SEPARATOR),
+  printScoreBoard: () =>
+    print(PRINT.NEWLINE_SEPARATOR + PRINT.EXECUTION_RESULT_TITLE),
+  printExecutionResult: (executionResult) => print(executionResult),
   printWinner: (winner) => print(PRINT.WINNER_PREFIX + winner),
   printError: (errorMessage) => print(errorMessage),
 };
