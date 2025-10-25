870af10b5c3de3b0be3ad2be941dfb2dd78dc916
HareuBang <divjae7124@gmail.com>
2025-10-26 04:18:49 +0900

    fix: CarFactory 테스트 코드에서 변경된 팩토리 메서드 적용

--- __tests__/CarFactoryTest.js
+++ __tests__/CarFactoryTest.js
@@ -15,7 +15,7 @@ describe("CarFactory를 통해 생성된 Car 동작 확인", () => {
     ])(
       "%s의 이름으로 자동차를 생성 시 이름과 거리가 올바르게 초기화된 Car 인스턴스가 생성된다.",
       (inputName) => {
-        const car = CarFactory.createCar(inputName);
+        const car = CarFactory.create(inputName);
 
         expect(car.getName()).toBe(inputName);
         expect(car.getDistance()).toBe(0);
@@ -25,7 +25,7 @@ describe("CarFactory를 통해 생성된 Car 동작 확인", () => {
 
   describe("자동차 전진 기능 테스트", () => {
     test("생성된 자동차를 전진시키면 이동거리가 1 증가합니다.", () => {
-      const car = CarFactory.createCar("pobi");
+      const car = CarFactory.create("pobi");
 
       car.forward();
       expect(car.getDistance()).toBe(1);
@@ -49,24 +49,20 @@ describe("CarFactory를 통해 생성된 Car 동작 확인", () => {
     ])(
       `잘못된 자동차 이름 "%s"을(를) 입력하면 ${CAR_NAME_ERROR_NOT_STRING}에러를 반환한다.`,
       (name) =>
-        expect(() => CarFactory.createCar(name)).toThrow(
-          CAR_NAME_ERROR_NOT_STRING
-        )
+        expect(() => CarFactory.create(name)).toThrow(CAR_NAME_ERROR_NOT_STRING)
     );
 
     test.each([[""], ["     "]])(
       `잘못된 자동차 이름 "%s"을(를) 입력하면 ${CAR_NAME_ERROR_NOT_EMPTY}에러를 반환한다.`,
       (name) => {
-        expect(() => CarFactory.createCar(name)).toThrow(
-          CAR_NAME_ERROR_NOT_EMPTY
-        );
+        expect(() => CarFactory.create(name)).toThrow(CAR_NAME_ERROR_NOT_EMPTY);
       }
     );
 
     test.each([["123456"], ["이것은 자동차 이름입니다."]])(
       `잘못된 자동차 이름 "%s"을(를) 입력하면 ${CAR_NAME_ERRORS.NOT_MAX_LENGTH_FIVE}에러를 반환한다.`,
       (name) => {
-        expect(() => CarFactory.createCar(name)).toThrow(
+        expect(() => CarFactory.create(name)).toThrow(
           CAR_NAME_ERRORS.NOT_MAX_LENGTH_FIVE
         );
       }
