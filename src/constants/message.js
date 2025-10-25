1feef1a617c8c19dcaad7a2574a825e647b0ebde
HareuBang <divjae7124@gmail.com>
2025-10-25 19:11:34 +0900

    refactor: 입력 메시지 관련 PROMPT  상수 구조 개선

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,8 @@
-const PARTICIPANT_CAR_NAMES_PROMPT =
-  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
-const RACE_LAPS_PROMPT = "시도할 횟수는 몇 회인가요?\n";
+export const PROMPT = {
+  PARTICIPANT_CAR_NAMES:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  RACE_LAPS: "시도할 횟수는 몇 회인가요?\n",
+};
 
 export const PRINT = {
   EXECUTION_RESULT_TITLE: "실행 결과",
@@ -8,4 +10,4 @@ export const PRINT = {
   WINNER_PREFIX: "최종 우승자 : ",
 };
 
-export { PARTICIPANT_CAR_NAMES_PROMPT, RACE_LAPS_PROMPT, PRINT };
+export { PROMPT, PRINT };
