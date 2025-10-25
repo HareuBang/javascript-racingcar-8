6649e1b5448e4701365a3bda6c19e81c69573813
HareuBang <divjae7124@gmail.com>
2025-10-26 01:12:15 +0900

    feat: RaceTrack의 경주 실행 흐름 및 출력 로직 구현
    - 예외 처리, 경주 진행, 경주 실행 결과 출력, 우승자 출력으로 전체적인 경주 실행
    흐름 로직을 구현했습니다.

--- src/RaceTrack.js
+++ src/RaceTrack.js
@@ -1,16 +1,48 @@
 export class RaceTrack {
   #input;
   #race;
+  #output;
 
-  constructor(input, race) {
+  constructor(input, race, output) {
     this.#input = input;
     this.#race = race;
+    this.#output = output;
+  }
+
+  #formatRaceProgress() {
+    const progress = this.#race.getRaceProgress();
+    return progress.map(
+      ({ name, distance }) => `${name} : ${"- ".repeat(distance)}`
+    );
+  }
+
+  #renderExecutionResult() {
+    const executionResult = this.#formatRaceProgress();
+    this.#output.printExecutionResult(executionResult);
+  }
+
+  #renderWinner() {
+    const winner = this.#race.determineWinner().join(", ");
+    this.#output.printWinner(winner);
   }
 
   async race() {
     const inputCarNames = this.#input.readCarNames();
     const inputRaceLaps = this.#input.readRaceLaps();
 
-    this.#race.prepare(inputCarNames, inputRaceLaps);
+    try {
+      this.#race.prepare(inputCarNames, inputRaceLaps);
+    } catch (error) {
+      this.#output.printError(error.message);
+      throw error;
+    }
+
+    this.#output.printScoreBoard();
+    while (this.#race.runNextLaps()) {
+      this.#race.start();
+      this.#renderExecutionResult();
+    }
+
+    this.#renderWinner();
   }
 }
