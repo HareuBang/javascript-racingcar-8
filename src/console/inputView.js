e6549d5f906b554f01f52617d6a46efe161f23f6
HareuBang <divjae7124@gmail.com>
2025-10-26 16:46:33 +0900

    refactor: inputView에 불필요한 async, await 제거
    - inputView.js는 Promise를 반환하는 것이 목적이기 때문에 async, await를 중
    복해서 사용하지 않아도 된다. MissionUtils.Console.readLineAsync(question),
    readLineAsync()는 Promise를 반환하기만 하면 된다.
    - 값을 즉시 사용하거나 다음 코드 순서를 await로 제어할 필요가 없는 곳에서는 불
    필요하다.

--- src/console/inputView.js
+++ src/console/inputView.js
@@ -1,12 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { PROMPT } from "../constants/message.js";
 
-const readLineAsync = async (question) =>
-  await MissionUtils.Console.readLineAsync(question);
+const readLineAsync = (question) =>
+  MissionUtils.Console.readLineAsync(question);
 
 const inputView = {
-  readCarNames: async () => await readLineAsync(PROMPT.PARTICIPANT_CAR_NAMES),
-  readRaceLaps: async () => await readLineAsync(PROMPT.RACE_LAPS),
+  readCarNames: () => readLineAsync(PROMPT.PARTICIPANT_CAR_NAMES),
+  readRaceLaps: () => readLineAsync(PROMPT.RACE_LAPS),
 };
 
 export default inputView;
